9c3792d33d26866301e8cc6404476a690ec90257
Hisni Mohammed <hisni@users.noreply.github.com>
2018-12-24 13:42:04 +0530

    Initial code

---
+++ Lab5/TicTacToe.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+
+public class TicTacToe implements ActionListener{
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public void actionPerformed(ActionEvent event){
+
+    }
+
+
+
+}
